a0ae39b62cfe045f2ece6a60b0f4b489af11b4ba
jeuxjeux20 <jeuxjeux20@outlook.fr>
2018-01-24 20:31:20 +0100

    progress bar increments correctly, passes alerady downloaded files

--- McDotNet/MainWindow.xaml.cs
+++ McDotNet/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
 using System.Net;
+using System.IO;
 
 namespace McDotNet
 {
@@ -25,7 +26,7 @@ namespace McDotNet
     {
         public MainWindow()
         {
-            if (!System.IO.Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\")) System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\");
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\")) Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\");
             InitializeComponent();
         }
         private string Version { get; set; } = "1.12.2";
@@ -35,7 +36,7 @@ namespace McDotNet
         private async void Button_ClickAsync(object sender, RoutedEventArgs e)
         {
 
-            WebClient DOWNLOADER = new WebClient();
+            WebClient downloader = new WebClient();
             PlayButton.IsEnabled = false;
             isWorking = true;
             StatusContainer.Visibility = Visibility.Visible;
@@ -46,15 +47,33 @@ namespace McDotNet
             {
                 await ChangeProgress("Deserializing libraries data...", 10);
                 VersionData = JsonConvert.DeserializeObject<Data.MinecraftVersion>(await response.Content.ReadAsStringAsync());
-                if (!System.IO.Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\" + Version + "\\libs\\")) System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\" + Version + "\\libs\\");
+                var path = Environment.GetFolderPath(
+                        Environment.SpecialFolder.ApplicationData)
+                    + "\\.mcdotnet\\versions\\" + Version + "\\libs\\";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                int downloadAdd = 80;
+                float incrementValue =  downloadAdd / VersionData.Libraries.Count; // progress bar goes 90
                 foreach (var library in VersionData.Libraries)
                 {
                     Uri uri = new Uri(library.Download.Artifact.Url);
-                    await ChangeProgress("Downloading "+ System.IO.Path.GetFileName(uri.LocalPath), StatusBar.Value+0.80);
-                    await DOWNLOADER.DownloadFileTaskAsync(library.Download.Artifact.Url, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\" + Version + "\\libs\\" + System.IO.Path.GetFileName(uri.LocalPath));
+                    var completePath = path + System.IO.Path.GetFileName(uri.LocalPath);
+                    var newBarValue = StatusBar.Value + incrementValue;
+                    if (!File.Exists(completePath))
+                    {
+                        await ChangeProgress("Downloading " + System.IO.Path.GetFileName(uri.LocalPath), StatusBar.Value + incrementValue);
+                        await downloader.DownloadFileTaskAsync(library.Download.Artifact.Url, completePath);
+                    } 
+                    else
+                    {
+                        await ChangeProgress(progress: newBarValue);
+                    }
                 }
                 await ChangeProgress("Downloading Minecraft...", StatusBar.Value+5);
-                await DOWNLOADER.DownloadFileTaskAsync("http://s3.amazonaws.com/Minecraft.Download/versions/" + Version + "/" + Version + ".jar", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\" + Version + "\\" + Version + ".jar");
+                await downloader.DownloadFileTaskAsync("http://s3.amazonaws.com/Minecraft.Download/versions/" + Version + "/" + Version + ".jar",
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.mcdotnet\\versions\\" + Version + "\\" + Version + ".jar");
                 await ChangeProgress("Applying Stuff...", StatusBar.Value + 5);
                 await ChangeProgress("Success !", 100);
                 
