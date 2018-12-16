dd90e52544875e1074de05783431094030f5c7c6
cth103 <cthdev@email.com>
2018-12-16 20:06:38 +0100

    add version manifest fix

---
+++ McDotNet/Data/ManifestVersion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDotNet.Data
+{
+    public class ManifestVersion
+    {
+        public string Id { get; set; } // lol noob version
+        public string Type { get; set; } // TODO : make this an enum
+        public string Url { get; set; } // lol noob url
+        public DateTime Time { get; set; } // lol noob time
+        public DateTime ReleaseTime { get; set; } // lol noob reles tim
+    }
+}
